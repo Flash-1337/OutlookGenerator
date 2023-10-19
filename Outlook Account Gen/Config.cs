b445403a94698cd7495833bc3afa3b7b045777b2
Flash <stop@doxxing.me>
2023-10-18 19:32:20 -0500

    Captcha Solver

---
+++ Outlook Account Gen/Config.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outlook_Account_Gen
+{
+    public class ExtensionJsonConfig
+    {
+        public string apiKey { get; set; }
+        public string appId { get; set; }
+        public bool useCapsolver { get; set; }
+        public bool manualSolving { get; set; }
+        public string solvedCallback { get; set; }
+        public bool useProxy { get; set; }
+        public string proxyType { get; set; }
+        public string hostOrIp { get; set; }
+        public string port { get; set; }
+        public string proxyLogin { get; set; }
+        public string proxyPassword { get; set; }
+        public bool enabledForBlacklistControl { get; set; }
+        public List<object> blackUrlList { get; set; }
+        public bool enabledForRecaptcha { get; set; }
+        public bool enabledForRecaptchaV3 { get; set; }
+        public bool enabledForHCaptcha { get; set; }
+        public bool enabledForFunCaptcha { get; set; }
+        public bool enabledForImageToText { get; set; }
+        public string reCaptchaMode { get; set; }
+        public string hCaptchaMode { get; set; }
+        public int reCaptchaDelayTime { get; set; }
+        public int hCaptchaDelayTime { get; set; }
+        public int textCaptchaDelayTime { get; set; }
+        public int reCaptchaRepeatTimes { get; set; }
+        public int reCaptcha3RepeatTimes { get; set; }
+        public int hCaptchaRepeatTimes { get; set; }
+        public int funCaptchaRepeatTimes { get; set; }
+        public int textCaptchaRepeatTimes { get; set; }
+        public string textCaptchaSourceAttribute { get; set; }
+        public string textCaptchaResultAttribute { get; set; }
+    }
+
+    public class Config
+    {
+        public static ExtensionJsonConfig GetConfig(string path)
+        {
+            if (!File.Exists(path))
+                return null;
+
+            string contents = File.ReadAllText(path);
+
+            return JsonConvert.DeserializeObject<ExtensionJsonConfig>(contents);
+        }
+
+        public static void SetConfig(string path, ExtensionJsonConfig config)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+        }
+    }
+}
--- Outlook Account Gen/Program.cs
+++ Outlook Account Gen/Program.cs
@@ -1,4 +1,5 @@
-﻿using PuppeteerSharp;
+﻿using Newtonsoft.Json;
+using PuppeteerSharp;
 using PuppeteerSharp.Input;
 
 namespace Outlook_Account_Gen;
@@ -6,7 +7,6 @@ namespace Outlook_Account_Gen;
 abstract class Program
 {
     private static IPage? _page;
-    private const string OkButton = "#idSIButton9";
 
     private static  string GetRandomString(int length = 10, bool usePasswordChars = false)
     {
@@ -26,6 +26,7 @@ abstract class Program
         if (_page == null)
             return;
 
+
         await _page.MainFrame.WaitForSelectorAsync(selector);
         await _page.MainFrame.TypeAsync(selector, text);
         await _page.Keyboard.PressAsync(Key.Enter);
@@ -33,19 +34,51 @@ abstract class Program
 
     public static async Task Main(string[] args)
     {
-        using var browserFetcher = new BrowserFetcher();
+        Console.WriteLine("Loading extension, This must be in the same directory in a folder named 'CapSolver.Browser.Extension'");
+        string pathToExtension = Path.GetFullPath("CapSolver.Browser.Extension");
 
+        if (!Directory.Exists(pathToExtension))
+        {
+            Console.WriteLine("Extension must be downloaded and placed in the same folder, Check github for instructions");
+            Console.Read();
+            return;
+        }
+
+        string configPath = pathToExtension + "\\assets\\config.json";
+
+        // Parse config.json
+        ExtensionJsonConfig configJson = Config.GetConfig(configPath);
+
+        if (configJson == null)
+            throw new Exception("Failed to parse config.json");
+
+        if (configJson.apiKey == "")
+        {
+            Console.WriteLine("Enter apikey, if you do not have one enter 'no'");
+
+            string input = Console.ReadLine() ?? "no";
+
+            configJson.apiKey = input;
+        }
+
+        Config.SetConfig(configPath, configJson);
+
+        using var browserFetcher = new BrowserFetcher();
         Console.WriteLine("Attempting Chromium Download");
         await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
 
         var browser = await Puppeteer.LaunchAsync(new LaunchOptions
         {
-            Headless = false
+            Headless = false,
+            Args = new[]
+            {
+                $@"--disable-extensions-except=""{pathToExtension}""",
+                $@"--load-extension=""{pathToExtension}"""
+            }
         });
         Console.WriteLine("Chromium Downloaded");
 
         _page = await browser.NewPageAsync();
-
         await _page.GoToAsync("https://signup.live.com/signup");
 
         string email = $"F{GetRandomString()}@outlook.com";
@@ -61,12 +94,13 @@ abstract class Program
         await _page.Keyboard.PressAsync(Key.Enter);
         await _page.Keyboard.PressAsync(Key.ArrowDown);
         
+        Thread.Sleep(500); // Some errors here so I wrote this lol
+
         await _page.MainFrame.WaitForSelectorAsync("#BirthDay");
         await _page.MainFrame.ClickAsync("#BirthDay");
         await _page.Keyboard.PressAsync(Key.Enter);
         await _page.Keyboard.PressAsync(Key.ArrowDown);
         await TypeAndClickNext("#BirthYear", "2000");
-        Console.WriteLine("No captcha solver implemented, you are going to have to do this yourself");
 
         _page.DefaultTimeout = int.MaxValue - 1;
 
@@ -75,9 +109,17 @@ abstract class Program
         await _page.MainFrame.WaitForSelectorAsync(okButton);
         await _page.MainFrame.ClickAsync(okButton);
         await _page.Keyboard.PressAsync(Key.Enter);
-        
+
+
+        await _page.CloseAsync();
+        await _page.CloseAsync(); // Close twice because it opens 2 tabs for some reason
+        await browser.CloseAsync();
+
         Console.WriteLine("Email: " + email);
         Console.WriteLine("Password: " + password);
+
+        File.AppendAllText("accounts.txt", $"{email}:{password}\n");
+
         Console.ReadLine();
     }
 }
